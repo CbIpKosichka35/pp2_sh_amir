0fb102f122bd3df84ea53961cd1678aa1c6d3e43
Amir <cbipkosichka@gmail.com>
2026-01-24 17:02:50 +0500

    hello_world.py добавил в реп

--- Practice1/python basics/hello_world.py
+++ Practice1/python basics/hello_world.py
@@ -1,1 +1,25 @@
-print("Hello, World!")
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+# double quotes
+
+print("This will work!")
+print('This will also work!')
+
+# print without new line, i can do any separator like end="space"
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+print("Hello World!", end="space")
+print("I will print on the same line.")
+
+#print nums
+print(450000)
+
+#do math in print
+print (2**3)
+print(10-5)
+
+#mix text and nums
+print("Hello! I'm ", 18, " years old")
