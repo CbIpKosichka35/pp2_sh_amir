7b5deba6ef88dc7ed4252857f2fb787ff1ba6ac1
Amir <cbipkosichka@gmail.com>
2026-01-24 17:53:47 +0500

    data_type добавил в реп

---
+++ Practice1/python basics/data_type.py
@@ -0,0 +1,27 @@
+x = 10
+print(x)
+print(type(x))
+
+y = 20.5
+print(y)
+print(type(x))
+
+z = "Hello World"
+print(z)
+print(type(x))
+
+v = [1, 2, 3, 4]
+print(v)
+print(type(v))
+
+w = None
+print(w)
+print(type(w))
+
+t = True
+print(t)
+print(type(t))
+
+g = range(5)
+print(g)
+print(type(g))
