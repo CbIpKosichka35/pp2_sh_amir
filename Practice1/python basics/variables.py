7aa00f5355d0ecb85554b9deaad16c0e83bb2267
Amir <cbipkosichka@gmail.com>
2026-01-24 17:48:48 +0500

    variables добавил в реп

---
+++ Practice1/python basics/variables.py
@@ -0,0 +1,34 @@
+x = 5
+print(x)
+y = "Amir"
+print(y)
+
+#i can change variable
+x = "Oleg"
+print(x)
+
+#specify data type
+x = str(3)    # x will be '3'
+x = int(3)    # x will be 3
+x = float(3)  # x will be 3.0
+
+#single and double quotes
+v = "John"
+# is the same as
+v = 'John'
+
+#case senstive
+a = 10
+A = 12
+# a and A is different variables
+
+#assign few values
+banana_amount, apple_ammount = 5, 10
+print(banana_amount, apple_ammount)
+#i can also do
+banana_amount = apple_ammount
+print(banana_amount)
+
+#several variables in print
+print(y + v)
+print(a + A)
