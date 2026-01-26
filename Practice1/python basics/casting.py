12df3732b69b4cc0c6edec0526501ff425e18fee
Amir <cbipkosichka@gmail.com>
2026-01-26 12:47:34 +0500

    casting.py добавил в реп

---
+++ Practice1/python basics/casting.py
@@ -0,0 +1,26 @@
+#i can reassign datatypes
+
+#to int
+x = int(2.8) #result will be 2
+y = int(1)
+z = int("10")
+
+print(x, " ", y, " ", z)
+print(type(x), " ", type(y), " ", type(z))
+
+#to float
+x = float(2.34)
+y = float(1)
+z = float("11")
+w = float("11.1")
+
+print(x, " ", y, " ", z, " ", w)
+print(type(x), " ", type(y), " ", type(z), " ", type(w))
+
+#to string
+x = str(2.35)
+y = str(1)
+z = str(12)
+
+print(x, " ", y, " ", z)
+print(type(x), " ", type(y), " ", type(z))
