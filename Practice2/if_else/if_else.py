88d6b8492a408bb24c3b1960190f1d189e4b48ed
Amir <cbipkosichka@gmail.com>
2026-02-01 22:27:11 +0500

    закончил if else bracket и добавил в реп

---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,17 @@
+#full if else chain
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+#else is working if elif and if methods are not working
+#ex. without elif
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
