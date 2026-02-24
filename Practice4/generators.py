18b4b5e02771d0dd1ec52262d61745cf449dc23d
Amir Shmanov <cbipkosichka@gmail.com>
2026-02-24 12:21:44 +0500

    добавил generators в rep

--- Practice4/generators.py
+++ Practice4/generators.py
@@ -1,52 +1,28 @@
-#iterators
+def square(n):
+    for i in range(1, n+1):
+        yield i * i
+n = int(input())
 
-mytuple = ("apple", "banana", "cherry")
-myit = iter(mytuple)
+for i in square(n):
+    print(i)
 
-print(next(myit))
-print(next(myit))
-print(next(myit))
-#iter() creates iterator
-#next() switches on next element in array
+n = int(input())
+print(*(i for i in range(0, n + 1, 2)), sep=",")
 
-#string is also iteratable
-mystr = "banana"
-myit = iter(mystr)
+def devisible(n):
+    for i in range(n + 1):
+        if i % 3 == 0 and i%4 ==0:
+            yield i
+n=int(input())
+for i in devisible(n):
+    print(i)
 
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
-print(next(myit))
+def squares(a,b):
+    for i in range(a, b+1):
+        yield i * i
+a=list(map(int,input().split()))
+for i in squares(a[0],a[1]):
+    print(i)
 
-#iterators can be used in loops
-spring_semestr = ["Calc2", "Discrete Strutures", "History", "PP2", "English", "Physics"]
-
-for discipline in spring_semestr:
-    print(discipline, end=" ")
-
-mystr = "banana"
-
-for x in mystr:
-    print(x)
-
-# iterators in classes
-class MyNumbers:
-  def __iter__(self):
-    self.a = 1
-    return self
-
-  def __next__(self):
-    if self.a <= 20:
-      x = self.a
-      self.a += 1
-      return x
-    else:
-      raise StopIteration
-
-myclass = MyNumbers()
-myiter = iter(myclass)
-
-for x in myiter:
-  print(x)
+n=int(input())
+print(*(i for i in range(n, -1, -1)), sep=" ")
