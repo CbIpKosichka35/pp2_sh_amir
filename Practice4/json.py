d8cae44c82563984ae3425671408f4004c0bfc77
Amir Shmanov <cbipkosichka@gmail.com>
2026-02-24 12:23:25 +0500

    добавил json  в rep

--- Practice4/json.py
+++ Practice4/json.py
@@ -26,3 +26,18 @@ for i in squares(a[0],a[1]):
 
 n=int(input())
 print(*(i for i in range(n, -1, -1)), sep=" ")
+
+import json
+with open("sample-data.json") as f:
+    text = f.read()
+
+data = json.loads(text)
+
+print("Interface Status")
+print("="*80)
+print(f"{'DN':50} {'Description':20} {'Speed':8} {'MTU':5}")
+print("-"*80)
+
+for item in data["imdata"]:
+    a = item["l1PhysIf"]["attributes"]
+    print(f"{a['dn']:50} {a['descr']:20} {a['speed']:8} {a['mtu']}")
